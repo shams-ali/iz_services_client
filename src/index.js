9ce43fa3f33aba34cb984d28949229c948aa588e
Shams Ali <shams.ali0216@gmail.com>
2018-02-04 14:31:23 -0800

    (config) router and redux

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-theme.css';
-
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Provider } from 'react-redux';
+
+import store from './store';
+import routes from './routes';
+
+import './styles/main.css';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>{routes}</Provider>,
+  document.getElementById('root')
+);
+
 registerServiceWorker();
