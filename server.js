6a1baf433a3d26158a575340e6943a94314e4a9d
Shams Ali <shams.ali0216@gmail.com>
2018-03-05 03:14:35 -0800

    (debug) proxy

--- server.js
+++ server.js
@@ -3,12 +3,13 @@ require('dotenv').config();
 const express = require('express');
 const proxy = require('express-http-proxy');
 const url = require('url');
+const path = require('path');
 
 const { PORT, API_URL } = process.env;
 
 const app = express();
 
-app.use('/', express.static('build'));
+app.use(express.static('build'));
 
 // app.use('/api', proxy(`${API_URL}`));
 
@@ -22,5 +23,9 @@ app.use(
     }
   })
 );
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.listen(PORT, err => console.warn(err || `Listening on ${PORT}`));
